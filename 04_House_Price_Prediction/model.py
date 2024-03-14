4689ba374afca6d69d0a806cd0f4c69f5f1f6bc5
saurabhkumar.conn@gmail.com <saurabhkumar.conn@gmail.com>
2024-03-14 20:02:12 +0530

    house price prediction

--- 04_House_Price_Prediction/model.py
+++ 04_House_Price_Prediction/model.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import pickle
 
-df = pd.read_csv('Data/House_Price.csv')
+df = pd.read_csv('Data/House_Price.json')
 
 x=df['Area(in sq.ft)'].values.reshape(-1, 1)
 y=df['Price(in Rs.)'].values.reshape(-1, 1)
